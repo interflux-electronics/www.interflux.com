243e9a66de67673d436627ca24238f5fa3909e5c
Jan Werkhoven <jan.werkhoven@gmail.com>
2019-09-08 22:05:28 +1000

    Rewrite the scrape script to be asynchronous

--- scrape.js
+++ scrape.js
@@ -1,109 +1,159 @@
-const rp = require("request-promise");
-const fs = require("fs");
+const request = require("request-promise");
 const HTMLParser = require("node-html-parser");
-const prettier = require("prettier");
 const prettydiff = require("prettydiff");
-const mkdirp = require("mkdirp");
+const mkdir = require("mkdirp");
+const fs = require("fs");
 
-const domain = "http://www.interflux.com";
-const locales = ["/en", "/de"];
-const paths = [
-  "",
-  "/soldering-fluxes",
-  // "/solder-pastes",
-  // "/solder-wires",
-  // "/flux-sprayer",
-  // "/auxiliaries",
-  // "/wave-soldering",
-  // "/selective-soldering",
-  // "/reflow-soldering",
-  // "/rework-and-repair",
-  // "/pre-tinning",
-  // "/company",
-  // "/documents",
-  "/contact"
-];
+const init = async () => {
+  console.log(`------`);
+  console.log(`Scraping interflux.com`);
 
-const template = `
-{% extends "layout.html.njk" %}
-
-{% block meta %}
-  <title>{{pageTitle}}</title>
-  {# <meta name="description" content=""> #}
-{% endblock %}
-
-{% block bodyId %}{{bodyId}}{% endblock %}
-{% block bodyClass %}{{bodyClass}}{% endblock %}
-
-{% block main %}
-  {{pageHtml}}
-{% endblock %}
-`;
-
-function generateUrls() {
   const arr = [];
-  for (let i = 0; i < locales.length; i++) {
-    for (let ii = 0; ii < paths.length; ii++) {
-      const url = `${domain}${locales[i]}${paths[ii]}`;
-      const uri = `${locales[i]}${paths[ii]}`;
+
+  prepare(arr);
+
+  await scrape(arr);
+
+  build(arr);
+  clean(arr);
+  write(arr);
+
+  console.log(`------`);
+  console.log(`Done!`);
+  console.log(`------`);
+};
+
+const prepare = arr => {
+  const host = "http://www.interflux.com";
+  const locales = ["/en", "/de"];
+  const paths = [
+    "",
+    "/soldering-fluxes",
+    // "/solder-pastes",
+    // "/solder-wires",
+    // "/flux-sprayer",
+    // "/auxiliaries",
+    // "/wave-soldering",
+    // "/selective-soldering",
+    // "/reflow-soldering",
+    // "/rework-and-repair",
+    // "/pre-tinning",
+    // "/company",
+    // "/documents",
+    "/contact"
+  ];
+
+  locales.forEach(locale => {
+    paths.forEach(path => {
+      const url = `${host}${locale}${path}`;
+      const uri = `${locale}${path}`;
       arr.push({ url, uri });
-    }
-  }
-  return arr;
-}
-
-function fetch(urls) {
-  for (let i = 0; i < urls.length; i++) {
-    scrape(urls[i]);
-  }
-}
-
-function scrape(obj) {
-  const { url, uri } = obj;
-  console.log(`Scraping ${url}`);
-  rp(url)
-    .then(function(html) {
-      const dom = HTMLParser.parse(html);
-
-      const bodyId = uri.replace(/^\//g, "").replace("/", "-");
-      const bodyClass = dom.querySelector("body").classNames.join(" ");
-      const pageTitle = dom.querySelector("head title").rawText;
-      const pageHtml = dom.querySelector(".region.region-corpus").innerHTML;
-
-      const cleanHtml = prettier.format(pageHtml, { parser: "html" });
-
-      prettydiff.options.source = cleanHtml;
-      prettydiff.options.language = "nunjucks";
-      prettydiff.options.mode = "beautify";
-      prettydiff.options.indent_size = 2;
-
-      const cleanNunjucks = prettydiff();
-
-      const njk = template
-        .replace("{{bodyId}}", bodyId)
-        .replace("{{bodyClass}}", bodyClass)
-        .replace("{{pageTitle}}", pageTitle)
-        .replace("{{pageHtml}}", cleanNunjucks);
-
-      const dir = `src/html/pages${uri}`;
-      const dest = `src/html/pages${uri}/index.njk`;
-
-      mkdirp(dir, function(err) {
-        if (err) console.error(err);
-
-        console.log(`Created directory ${dir}`);
-
-        fs.writeFile(dest, njk, function(err) {
-          if (err) console.log(err);
-          console.log(`Created ${dest}`);
-        });
-      });
-    })
-    .catch(function(err) {
-      if (err) console.log(err);
     });
+  });
+
+  console.log(`------`);
+  console.log(`URLs to scrape: ${arr.length}`);
+  console.log(`------`);
+};
+
+async function scrape(arr) {
+  const promises = [];
+  arr.forEach(page => {
+    promises.push(fetch(page));
+  });
+  return Promise.all(promises);
 }
 
-const urls = generateUrls();
+async function fetch(page) {
+  console.log(`Scraping ${page.url}`);
 
-fetch(urls);
+  const success = html => {
+    console.log("COMPLETED", page.url);
+    page.dom = HTMLParser.parse(html);
+    page.id = page.uri.replace(/^\//g, "").replace("/", "-");
+    page.class = page.dom.querySelector("body").classNames.join(" ");
+    page.title = page.dom.querySelector("head title").rawText;
+    page.corpus = page.dom.querySelector(".region.region-corpus").innerHTML;
+    page.hasHighlight = page.dom.querySelector(".region.region-highlight")
+      ? true
+      : false;
+    page.hasFooter = page.dom.querySelector(".region.region-footer2")
+      ? true
+      : false;
+  };
+
+  const fail = err => {
+    console.error("FAILED", page.url, err);
+  };
+
+  return request(page.url)
+    .then(success)
+    .catch(fail);
+}
+
+const build = arr => {
+  console.log(`------`);
+  console.log(`Building Nunjuck templates (HTML) with scraped data`);
+
+  arr.forEach(page => {
+    page.html = `{% extends "layout.html.njk" %}
+
+    {% block meta %}
+      <title>${page.title}</title>
+      {# <meta name="description" content=""> #}
+    {% endblock %}
+
+    {% block bodyId %}${page.id}{% endblock %}
+    {% block bodyClass %}${page.class}{% endblock %}
+
+    {% block page %}
+      ${page.hasHighlight ? '{% include "partials/highlight.njk" %}' : ""}
+
+      <div class="region region-corpus">
+        <div class="inner-region">
+          ${page.corpus}
+        </div>
+      </div>
+
+      ${page.hasFooter ? '{% include "partials/footer.njk" %}' : ""}
+    {% endblock %}`;
+  });
+};
+
+const clean = arr => {
+  console.log(`------`);
+  console.log(`Cleaning up the HTML`);
+
+  prettydiff.options.mode = "beautify";
+  prettydiff.options.language = "nunjucks";
+  prettydiff.options.indent_size = 2;
+
+  arr.forEach(page => {
+    prettydiff.options.source = page.html;
+    page.clean = prettydiff();
+  });
+};
+
+const write = arr => {
+  console.log(`------`);
+  console.log(`Writing files`);
+
+  arr.forEach(page => {
+    const dir = `src/html/pages${page.uri}`;
+    const dest = `src/html/pages${page.uri}/index.njk`;
+
+    mkdir(dir, err => {
+      if (err) console.error(err);
+
+      console.log(`Created directory: ${dir}`);
+
+      fs.writeFile(dest, page.clean, err => {
+        if (err) console.log(err);
+
+        console.log(`Created file: ${dest}`);
+      });
+    });
+  });
+};
+
+init();
