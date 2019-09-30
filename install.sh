e615694fabe61bcec16307eeefaec5da20a900e5
Jan Werkhoven <jan.werkhoven@gmail.com>
2019-09-30 20:54:38 +1000

    Make the deploy scripts portable

--- install.sh
+++ install.sh
@@ -1,14 +1,12 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 set -e
 set -o pipefail
 
-bash
-
 # This hack makes the nvm binary available to this script.
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 BRANCH=$1
 REVISION=$2
