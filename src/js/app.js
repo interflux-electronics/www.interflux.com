25859dd3542127f0262bfc8196fb6816aea74541
Jan Werkhoven <jan.werkhoven@gmail.com>
2020-09-24 19:49:02 +1000

    Replace highlight banner with SMTA banner

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,13 @@
 import { documents } from "./functions.js";
 
+function hideHighlight() {
+  const el = document.querySelector(".region-highlight.hidden");
+  el.parentNode.removeChild(el);
+}
+
 function init() {
   documents();
+  hideHighlight();
 }
 
 document.addEventListener("DOMContentLoaded", function() {
