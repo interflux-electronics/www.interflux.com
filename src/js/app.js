0474bd7a0addfda7640bd591d18dafd7ad80c4be
Jan Werkhoven <jan.werkhoven@gmail.com>
2020-09-24 21:48:58 +1000

    Add SMTA modal

--- src/js/app.js
+++ src/js/app.js
@@ -5,9 +5,34 @@ function hideHighlight() {
   el.parentNode.removeChild(el);
 }
 
+function prepareBookModal() {
+  const banner = document.querySelector("a#smta");
+  const overlay = document.querySelector("#modal .overlay");
+
+  banner.onclick = event => {
+    event.preventDefault();
+    openModal();
+  };
+
+  overlay.onclick = () => {
+    closeModal();
+  };
+}
+
+function openModal() {
+  document.body.classList.add("show-modal");
+  document.querySelector("#page").style.top = `-${window.scrollY}px`;
+}
+
+function closeModal() {
+  document.body.classList.remove("show-modal");
+  document.querySelector("#page").removeAttribute("style");
+}
+
 function init() {
   documents();
   hideHighlight();
+  prepareBookModal();
 }
 
 document.addEventListener("DOMContentLoaded", function() {
