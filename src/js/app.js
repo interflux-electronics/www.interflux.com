b3ef3658f7edd6ad889a9bb728eda8492113b1ad
Jan Werkhoven <jan.werkhoven@gmail.com>
2021-01-30 19:13:35 +1100

    Fetch webinars from API

--- src/js/app.js
+++ src/js/app.js
@@ -1,69 +1,8 @@
-import { documents } from "./functions.js";
-
-function modals() {
-  const webinars = document.querySelector("#webinars");
-  const links = document.querySelectorAll("#webinars a");
-  const overlay = document.querySelector("#modal .overlay");
-
-  if (!webinars) {
-    return;
-  }
-
-  links.forEach(link => {
-    link.onclick = event => {
-      event.preventDefault();
-      openModal();
-    };
-  });
-
-  overlay.onclick = () => {
-    closeModal();
-  };
-}
-
-function openModal() {
-  document.body.classList.add("show-modal");
-  document.querySelector("#page").style.top = `-${window.scrollY}px`;
-}
-
-function closeModal() {
-  document.body.classList.remove("show-modal");
-  document.querySelector("#page").removeAttribute("style");
-}
-
-function banners() {
-  // const classList = document.body.classList;
-  // const isFluxPage = classList.contains("page-soldering-fluxes");
-  // const isWavePage = classList.contains("page-wave-soldering");
-  // const isPastePage = classList.contains("page-solder-pastes");
-  // const isReflowPage = classList.contains("page-reflow-soldering");
-
-  const smta = document.querySelector(".region-highlight #smta");
-  // const lmpa = document.querySelector(".region-highlight #lmpa");
-  const webinars = document.querySelector(".region-highlight #webinars");
-
-  // if (isFluxPage || isWavePage) {
-  //   // Show webinars banner on flux and wave pages
-  //   smta.parentNode.removeChild(smta);
-  //   lmpa.parentNode.removeChild(lmpa);
-  // } else if (isPastePage || isReflowPage) {
-  //   // Show SMTA banner on paste and reflow pages
-  //   webinars.parentNode.removeChild(webinars);
-  //   lmpa.parentNode.removeChild(lmpa);
-  // } else {
-  //   // In all other cases, show the LMPA banner
-  //   webinars.parentNode.removeChild(webinars);
-  //   smta.parentNode.removeChild(smta);
-  // }
-
-  webinars.parentNode.removeChild(webinars);
-  smta.parentNode.removeChild(smta);
-}
+import { renderBanners, renderDocumentLinks } from "./functions.js";
 
 function init() {
-  documents();
-  banners();
-  modals();
+  renderBanners();
+  renderDocumentLinks();
 }
 
 document.addEventListener("DOMContentLoaded", function() {
--- src/js/functions.js
+++ src/js/functions.js
@@ -1,38 +1,196 @@
-export async function documents() {
+const apiHost =
+  location.host === "interflux.com"
+    ? "https://api.interflux.com"
+    : "http://localhost:3000";
+
+const renderBanners = async () => {
+  const classList = document.body.classList;
+  const isContactPage = classList.contains("page-contact");
+
+  if (isContactPage) {
+    return document.querySelector(".region-highlight").classList.add("hidden");
+  }
+  // const isFluxPage = classList.contains("page-soldering-fluxes");
+  // const isWavePage = classList.contains("page-wave-soldering");
+  // const isPastePage = classList.contains("page-solder-pastes");
+  // const isReflowPage = classList.contains("page-reflow-soldering");
+
+  console.debug("renderBanners()");
+  const cache = sessionStorage.getItem("webinars");
+  console.debug({ cache });
+  const webinars = cache ? JSON.parse(cache) : await fetchWebinars();
+
+  console.debug({ webinars });
+
+  const loadBanner = document.querySelector(".region-highlight #loading");
+  const lmpaBanner = document.querySelector(".region-highlight #lmpa");
+  const webinarBanner = document.querySelector(".region-highlight #webinars");
+
+  if (webinars.length < 1) {
+    lmpaBanner.classList.remove("hidden");
+  } else {
+    webinarBanner.classList.remove("hidden");
+    populateWebinarModal(webinars);
+    bindModalEvents();
+  }
+
+  loadBanner.classList.add("hidden");
+};
+
+async function fetchWebinars() {
+  console.debug("fetchWebinars()");
+
+  const url = `${apiHost}/v1/public/webinars`;
+  const options = {
+    headers: {
+      "Content-Type": "application/vnd.api+json"
+    }
+  };
+  const response = await fetch(url, options);
+  const json = await response.json();
+
+  console.debug({ json });
+
+  const webinars = json.data.map(obj => {
+    const id = obj.id;
+    const attr = obj.attributes;
+    const title = attr.title || "TBC";
+    const topic =
+      attr.topic.replace(/\*([^*]+)\*/g, "<mark>$1</mark>") || "TBC";
+    const audience = attr.audience || "TBC";
+    const duration = attr.duration ? `~${attr.duration}min + Q&A` : "TBC";
+    const url = attr.url ? attr.url : "https://gotowebinar.com/";
+    const startTime = attr["start-time"];
+    const startTimeFormatted = new Date(startTime).toLocaleString(undefined, {
+      weekday: "long",
+      day: "numeric",
+      month: "short",
+      year: "numeric",
+      hour: "numeric",
+      hour12: false,
+      minute: "numeric",
+      timeZoneName: "short"
+    });
+
+    return {
+      id,
+      title,
+      topic,
+      audience,
+      duration,
+      url,
+      startTime,
+      startTimeFormatted
+    };
+  });
+
+  const now = Date.now();
+  const soonBeforeLater = (a, b) => {
+    if (a.startTime < b.startTime) {
+      return -1;
+    }
+    if (a.startTime > b.startTime) {
+      return 1;
+    }
+    return 0;
+  };
+
+  // Return only the webinars in the future and sort them soon before later.
+  const arr = webinars.filter(w => w.startTime > now).sort(soonBeforeLater);
+
+  // Cache the results so we don't have to fetch them later in this session.
+  // sessionStorage.setItem("webinars", JSON.stringify(arr));
+
+  return arr;
+}
+
+function populateWebinarModal(webinars) {
+  const modal = document.querySelector("#modal .content section");
+
+  webinars.forEach((webinar, i) => {
+    const div = document.createElement("div");
+    div.innerHTML = `
+      <hr>
+      <h2><span>${i + 1}. ${webinar.title}</span></h2>
+      <p>Who should attend: ${webinar.audience}</p>
+      <p>Topic: ${webinar.topic}</p>
+      <p>When:
+        <time>${webinar.startTimeFormatted}</time>
+      </p>
+      <p>Duration: ${webinar.duration}</p>
+      <a href="${
+        webinar.url
+      }" target="_blank" rel="nofollow noreferrer" class="button">
+        Register
+      </a>
+    `;
+    modal.append(div);
+  });
+}
+
+function bindModalEvents() {
+  console.debug("bindModalEvents()");
+
+  const webinars = document.querySelector("#webinars");
+  const links = document.querySelectorAll("#webinars a");
+  const overlay = document.querySelector("#modal .overlay");
+
+  if (!webinars) {
+    return;
+  }
+
+  links.forEach(link => {
+    link.onclick = event => {
+      event.preventDefault();
+    };
+  });
+
+  webinars.onclick = () => {
+    openModal();
+  };
+
+  overlay.onclick = () => {
+    closeModal();
+  };
+}
+
+function openModal() {
+  document.body.classList.add("show-modal");
+  document.querySelector("#page").style.top = `-${window.scrollY}px`;
+}
+
+function closeModal() {
+  document.body.classList.remove("show-modal");
+  document.querySelector("#page").removeAttribute("style");
+}
+
+async function renderDocumentLinks() {
+  console.debug("renderDocumentLinks()");
+
   const isProductPage = document.body.classList.contains("page-products");
 
   if (!isProductPage) {
     return;
   }
 
-  console.log("documents() fetching...");
+  const cache = sessionStorage.getItem("documents");
 
-  const host =
-    location.host === "interflux.com"
-      ? "https://api.interflux.com"
-      : "http://localhost:3000";
+  console.debug({ cache });
 
-  const url = `${host}/v1/public/documents/?include=cdn_files`;
-  const options = {
-    headers: {
-      "Content-Type": "application/vnd.api+json"
-    }
-  };
-
-  const response = await fetch(url, options);
-  const json = await response.json();
+  const json = cache ? JSON.parse(cache) : await fetchDocuments();
   const { data, included } = json;
 
   const TDs = data.filter(TD => {
     return TD.relationships["document-category"].data.id === "TD";
   });
-  const names = TDs.map(TD => {
-    return TD.attributes.name;
-  });
-  const uniques = [...new Set(names)];
-  const sorted = uniques.sort();
 
-  console.log(sorted);
+  // const names = TDs.map(TD => {
+  //   return TD.attributes.name;
+  // });
+  // const uniques = [...new Set(names)];
+  // const sorted = uniques.sort();
+
+  console.debug({ TDs });
 
   const productRows = document.querySelectorAll(".product-row");
 
@@ -42,7 +200,7 @@ export async function documents() {
     const matches = TDs.filter(TD => {
       return TD.id.startsWith(`documents/products/${kebabName}/`);
     });
-    console.log("documents()", name, matches);
+    console.debug(name, { matches });
     const details = productRow.querySelector(".product-details");
     const documents = productRow.querySelectorAll(".product-document-link");
 
@@ -93,3 +251,24 @@ export async function documents() {
     });
   });
 }
+
+async function fetchDocuments() {
+  console.debug("fetchDocuments() fetching...");
+
+  const url = `${apiHost}/v1/public/documents/?include=cdn_files`;
+  const options = {
+    headers: {
+      "Content-Type": "application/vnd.api+json"
+    }
+  };
+
+  const response = await fetch(url, options);
+  const json = await response.json();
+
+  // Cache the results so we don't have to fetch them later in this session.
+  sessionStorage.setItem("documents", JSON.stringify(json));
+
+  return json;
+}
+
+export { renderBanners, renderDocumentLinks };
